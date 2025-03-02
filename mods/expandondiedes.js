c46af40dce837cacbb1805d6acf8e0db4921248b
thegaemer100 <aemory.phd@gmail.com>
2025-03-02 12:15:50 -0500

    Update and rename wtfium.js to expandondiedes.js

---
+++ mods/expandondiedes.js
@@ -0,0 +1,20 @@
+elements.fallium = {
+  color: ["#ffffff","#ffff00","#ff0000","#f18b2e"],
+  behavior: behaviors.POWDER,
+  category: "expandondiedes",
+  state: "solid",
+  density: 265,
+  burn: 10000,
+  burnTime: 300,
+  burnInto: ["carbon_dioxide"],
+};
+elements.observium = {
+  color: ["#000000","#813e72","#ad699e","#ff9ae9"],
+  behavior: behaviors.POWDER,
+  category: "expandondiedes",
+  state: "solid",
+  density: 265,
+  burn: 9999,
+  burnTime: 300,
+  burnInto: ["carbon_dioxide"],
+};
