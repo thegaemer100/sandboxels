47acdb8841bb82800605bc98cbba3cab3979f915
thegaemer100 <aemory.phd@gmail.com>
2025-03-02 12:23:32 -0500

    Update and rename expandondiedes.js to expandonides.js

---
+++ mods/expandonides.js
@@ -0,0 +1,40 @@
+elements.fallium = {
+  color: ["#ffffff","#ffff00","#ff0000","#f18b2e"],
+  behavior: behaviors.POWDER,
+  category: "expandonides",
+  state: "solid",
+  density: 265,
+  burn: 10000,
+  burnTime: 300,
+  burnInto: ["carbon_dioxide"],
+};
+elements.observium = {
+  color: ["#000000","#813e72","#ad699e","#ff9ae9"],
+  behavior: behaviors.POWDER,
+  category: "expandonides",
+  state: "solid",
+  density: 265,
+  burn: 9999,
+  burnTime: 300,
+  burnInto: ["carbon_dioxide"],
+};
+elements.moltium = {
+  color: ["#000000","#f0f0f0","#ffffff"],
+  behavior: behaviors.POWDER,
+  category: "expandonides",
+  state: "solid",
+  density: 265,
+  burn: 20000,
+  burnTime: 400,
+  burnInto: ["carbon_dioxide"],
+};
+elements.wailium = {
+  color: ["#a5a672","#2b0b08","#777736","#7a0c09"],
+  behavior: behaviors.POWDER,
+  category: "expandonides",
+  state: "solid",
+  density: 265,
+  burn: 1500,
+  burnTime: 300,
+  burnInto: ["carbon_dioxide"],
+};
