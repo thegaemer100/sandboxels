c030ad55354b07c74839e7d4216efc9985d7c557
thegaemer100 <aemory.phd@gmail.com>
2025-03-02 12:03:26 -0500

    Create wtfium.js

---
+++ mods/wtfium.js
@@ -0,0 +1,13 @@
+elements.rutherfordium = {
+    category: "wtfium n shit",
+    state: "powder",
+    tempHigh: 10000,
+    stateHigh: "molten_wtfium",
+    density: 20000,
+    color: ["#FF0000", "#00FF00", "#0000FF"],
+    behavior: [
+        "XX|CR:radiation%5 AND CR:fallout%0.3|XX",
+        "CR:radiation%5 AND CR:fallout%0.3|XX|CR:radiation%5 AND CR:fallout%0.3",
+        "M2|M1 AND CR:radiation%5 AND CR:fallout%0.3|M2",
+    ],
+}
